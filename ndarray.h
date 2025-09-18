195649e4b21e196708e86fb8928691ccd5d19c8d
Parkman145 <ParkerLearmonth@gmail.com>
2025-09-17 21:26:47 -0700

    Implement and format constructors

--- ndarray.h
+++ ndarray.h
@@ -12,8 +12,8 @@ private:
   int compute_index(const std::vector<int> &location) const;
 
 public:
-  Ndarray(const std::vector<int>& shape);
-  
+  Ndarray(const std::vector<int> &shape);
+  Ndarray(const std::vector<int> &shape, const std::vector<T> &data);
 
   T &operator[](const std::vector<int> &location);
   
@@ -23,15 +23,26 @@ public:
 
 
 template <typename T>
-Ndarray<T>::Ndarray(const std::vector<int>& shape) {
-    int size = vector_utils::product(shape);
-    
-    data.resize(size);
-    this->shape = shape;
+Ndarray<T>::Ndarray(const std::vector<int> &shape)
+{
+  int size = vector_utils::product(shape);
+
+  data.resize(size);
+  this->shape = shape;
 }
 
+template <typename T>
+Ndarray<T>::Ndarray(const std::vector<int> &shape, const std::vector<T> &data)
+{
+  int size = vector_utils::product(shape);
 
+  if (data.size() != vector_utils::product(shape))
+  {
+    throw InvalidShapeException();
+  }
 
-
+  this->data = data;
+  this->shape = shape;
+}
 
 #endif
